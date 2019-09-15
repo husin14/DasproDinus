3701798a091c10fbaf44f919b855177ef168f47e
husin14 <54317405+husin14@users.noreply.github.com>
2019-09-15 08:44:34 +0700

    Add files via upload

---
+++ TUGAS DASPRO 7.1/nloop1/main.c
@@ -0,0 +1,23 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Nama = Husin Sufi
+// NIM = A11.2018.10909
+// Kelompok = A11.4102
+
+int main()
+{
+    int a;  //// int a sebagai iterator
+
+    printf("perulangan dengan menggunakan FOR\n");
+
+    for (a=1; a<=7; a++) ////i=0 sebagai inisialisasi nilai awal dan i<=7 sebagai batasan perulangan i++ sebagai pembaruan nilai
+
+    {
+        printf("perulangan ke-%d\n",a);
+    }
+
+    return 0;
+}
---
+++ TUGAS DASPRO 7.1/nloop8/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Nama = Husin Sufi
+// NIM = A11.2018.10909
+// Kelompok = A11.4102
+
+int main()
+{
+    int x,y;
+
+x=1;
+while (x<=7)
+{
+       printf("\t\t perulangan ke- %d\n",x);
+    y=1;
+    while (y<=5)
+    {
+         printf("angka ke- %d\n",y);
+
+         y=y+1;
+    }
+    x=x+1;
+
+}
+    return 0;
+}
+
