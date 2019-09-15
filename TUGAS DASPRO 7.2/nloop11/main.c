65f768435e633cd9e5bbe54851ec6a3fdd159f49
husin14 <54317405+husin14@users.noreply.github.com>
2019-09-15 08:47:00 +0700

    Add files via upload

---
+++ TUGAS DASPRO 7.2/nloop11/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Nama = Husin Sufi
+// NIM = A11.2018.10909
+// Kelompok = A11.4102
+
+int main()
+{
+    int x,y;
+x=1;
+do
+{
+       printf("\t\t perulangan ke- %d\n",x);
+       x=x+1;
+
+
+y=1;
+while (y<=5)
+
+   {
+       printf("angka ke %d\n",y);
+       y=y+1;
+   }
+
+}
+while (x<=7);
+
+    return 0;
+}
+
---
+++ TUGAS DASPRO 7.2/nloop14/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Nama = Husin Sufi
+// NIM = A11.2018.10909
+// Kelompok = A11.4102
+
+int main()
+{
+    int x,y;
+
+    for (x=1; x<7; x++) ///OUTER LOOP
+{
+
+
+    for (y=1;y<7;y++) ///INNER LOOP
+    {
+
+
+       printf(" %d%d ",x,y);
+    }
+        printf("\n");
+}
+
+    return 0;
+}
